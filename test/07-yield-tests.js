1cd1a55dd10d7c2df0e98432303fa9ab89ef30d3
mished <mishated@gmail.com>
2016-03-06 01:23:06 +0300

    Fix getFibonacciSequence test

--- test/07-yield-tests.js
+++ test/07-yield-tests.js
@@ -245,7 +245,7 @@ describe('07-yield-tasks', function() {
             );
             if (index>=expected.length) break;
         }
-
+        assert.equal(index, expected.length, 'getFibonacciSequence should yield elements');
     });
 
 
