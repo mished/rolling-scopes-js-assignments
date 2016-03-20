df6ed85ba2658017160f654fdce921f0d001a871
Misha Trotsiuk <mished@users.noreply.github.com>
2016-03-20 23:13:19 +0300

    Fix #6

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -57,14 +57,9 @@ function parseDataFromIso8601(value) {
  */
 function isLeapYear(date) {
     const year = date.getFullYear();
-    if (year % 4 !== 0) {
-        return false;
-    } else if (year % 100 !== 0) {
-        return true;
-    } else if (year % 400 !== 0) {
-        return false;
-    }
-    return true;
+    return year % 4 === 0
+        && year % 100 !== 0
+        || year % 400 === 0;
 }
 
 
